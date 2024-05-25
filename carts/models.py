a03e5fbb1d81f77beeb7c0640f2baaec83c16d76
David Truman <onwukadavid21@gmail.com>
2024-05-25 13:02:06 +0100

    update: add cart model

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from products.models import Product
 
-# Create your models here.
+user = get_user_model()
+
+class Cart(models.Model):
+    user = models.OneToOneField(user, on_delete=models.DO_NOTHING)
+    product = models.ManyToManyField(to=Product)
+
+    def __str__(self):
+        return f'{user.username}_cart'
+
+    class Meta:
+        db_table = 'carts'
